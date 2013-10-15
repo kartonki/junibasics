d74f1e2c3b9078267a5922ba015ac44d7a0f635f
jorge <jorge@KartBuntu>
2013-10-15 19:14:59 +0300

    Changes on Multiply method

--- junitbasics/src/main/java/com/kartonki/junitbasics/App.java
+++ junitbasics/src/main/java/com/kartonki/junitbasics/App.java
@@ -4,11 +4,25 @@ package com.kartonki.junitbasics;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
         //Sending console output
+        
     }
+
+    /**
+     *
+     */
+    //public class Multiplex {
+
+        public int multiply(int x, int y) {
+            // the following is just an example
+            if (x > 999) {
+                throw new IllegalArgumentException("X should be less than 1000");
+            }
+            return x * y;
+        }
+    //}
 }
--- junitbasics/src/test/java/com/kartonki/junitbasics/AppTest.java
+++ junitbasics/src/test/java/com/kartonki/junitbasics/AppTest.java
@@ -7,32 +7,57 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest
+        extends TestCase {
+
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        assertTrue(true);
+    }
+
+    /**
+     * Test of main method, of class App.
+     */
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        App.main(args);
+        App tester= new App();
+        //   Integer result =    tester.multiply(10, 5);
+        //Integer result;
+        //result = tester.multiply(3, 2);
+        // TODO review the generated test code and remove the default call to fail.
+        assertEquals("10 x 5 must be 50", 50, tester.multiply(10, 5));
+        //fail("The test case is a prototype.");
+        //MyClass tester = new MyClass();
+        //assertEquals("10 x 5 must be 50", 50, tester.multiply(10, 5));
     }
 }
